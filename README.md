c5a1a287a610ebb526e928d5e01e8913bf1bb1ce
Shivi Vats <shivivats7@gmail.com>
2024-08-28 10:13:58 +0200

    removed the files that should've been gitignored this whole time

--- README.md
+++
@@ -1,5 +0,0 @@
-# Watermelon Game
-
-A clone of the Suika Game aimed for Android platforms. Made using Godot.
-
-I'm using this as a project to learn Godot with.
--- scripts/points_label.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-	GameManager.points_updated.connect(points_updated)
-
-func points_updated(new_points : int):
-	self.text = "Points : " + str(new_points)
